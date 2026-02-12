f2847980428e23b2d0ad89d8ae201ecbb2624aed
Георги Илиев <georgyiliev4@gmail.com>
2026-02-12 20:23:00 +0200

    used std::forward to reduce code repetition

--- Ion.Core/Threading/SafeQueue.h
+++ Ion.Core/Threading/SafeQueue.h
@@ -8,97 +8,88 @@
 #include <stdexcept>
 #include <utility>
 
-
-
-template<typename T>
-requires std::movable<T>
-class SafeQueue
+namespace Ion::Threading
 {
-public:
 
-	
-	
-	void push(const T& t) requires std::copyable<T>
+
+	template<typename T>
+		requires std::movable<T>
+	class SafeQueue
 	{
-		{
-			const std::lock_guard guard(m_mutex);
+	public:
 
-			if (stop)
+
+		 template<typename U>
+		 requires std::convertible_to<U,T>
+		 bool push(U&& u) {
+
 			{
-				throw std::runtime_error("Queue is stopped");
+				const std::lock_guard guard(m_mutex);
+
+				if (stop)
+				{
+					return false;
+				}
+
+
+				m_queue.push(std::forward<U>(u));
 			}
 
-			m_queue.push(t);
+			m_conditionVariable.notify_one();
+
+			return true;
+
 		}
 
-		m_conditionVariable.notify_one();
-	}
 
-	void push(T&& t) {
-
+		[[nodiscard]] std::optional<T> pop()
 		{
-			const std::lock_guard guard(m_mutex);
+			std::unique_lock<std::mutex> lock(m_mutex);
 
-			if (stop)
+			//wait until the queue is not empty OR we have stopped the queue
+			//  !m_queue.empty() means no data to use
+			//  stop means we are done with data
+			m_conditionVariable.wait(lock, [this] { return !m_queue.empty() || stop; });
+
+			if (m_queue.empty())
 			{
-				throw std::runtime_error("Queue is stopped");
+				return std::nullopt;
+			}
+			else
+			{
+				//construct a std::optional with the value to be returned
+				auto optVal = std::make_optional<T>(std::move(m_queue.front()));
+
+				//pop the internal queue
+				m_queue.pop();
+
+				return optVal;
+
 			}
 
 
-			m_queue.push(std::move(t));
 		}
 
-		m_conditionVariable.notify_one();
-
-	}
-
-
-	[[nodiscard]] std::optional<T> pop()
-	{
-		std::unique_lock<std::mutex> lock(m_mutex);
-
-		//wait until the queue is not empty OR we have stopped the queue
-		//  !m_queue.empty() means no data to use
-		//  stop means we are done with data
-		m_conditionVariable.wait(lock, [this] { return !m_queue.empty() || stop; });
-
-		if (m_queue.empty())
+		void shutdown()
 		{
-			return std::nullopt;
-		}
-		else
-		{
-			//construct a std::optional with the value to be returned
-			auto optVal = std::make_optional<T>(std::move(m_queue.front()));
-
-			m_queue.pop();
-
-			return optVal;
-
+			std::unique_lock<std::mutex> lock(m_mutex);
+			stop = true;
+			lock.unlock();
+			m_conditionVariable.notify_all();
 		}
 
-
-	}
-
-	void shutdown()
-	{
-		std::unique_lock<std::mutex> lock(m_mutex);
-		stop = true;
-		lock.unlock();
-		m_conditionVariable.notify_all();
-	}
-
-private:
+	private:
 
 
 
-	std::mutex m_mutex;
-	std::condition_variable m_conditionVariable;
+		std::mutex m_mutex;
+		std::condition_variable m_conditionVariable;
 
-	bool stop = false;
-	std::queue<T> m_queue;
+		bool stop = false;
+		std::queue<T> m_queue;
 
-	
 
-};
 
+	};
+
+}
