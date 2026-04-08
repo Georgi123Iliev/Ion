2af51d1925c7fff8abb60841cfc0c431d1747745
Георги Илиев <georgyiliev4@gmail.com>
2026-04-08 21:50:09 +0300

    Moved the http connection loop into http session

--- Ion.Core/Net/HTTP/HTTPServer.hpp
+++ Ion.Core/Net/HTTP/HTTPServer.hpp
@@ -7,14 +7,17 @@
 #include "Threading/ThreadPool.hpp"
 #include "Net/NetworkEnvironment.hpp"
 #include "Net/TCP/TcpAcceptor.hpp"
-#include "Net/HTTP/Routing/Router.hpp"
 
+#include "Net/HTTP/HttpSession.hpp"
+
+#include "Net/Constants.hpp"
 namespace Ion::Net::HTTP
 {
+	template<typename RouterT>
 	class HttpServer
 	{
 	public:
-		HttpServer(const NetworkEnvironment& env, Router<>&& router)
+		HttpServer(const NetworkEnvironment& env, RouterT&& router)
 			: m_router(std::move(router)), env(&env), m_connectionAcceptor(env)
 		{
 		}
@@ -24,7 +27,7 @@ namespace Ion::Net::HTTP
 		
 
 	private:
-		Router<> m_router;
+		RouterT m_router;
 		Threading::ThreadPool m_threadPool;
 		/*
 		* env must preceed m_connectionAcceptor
@@ -32,4 +35,31 @@ namespace Ion::Net::HTTP
 		const NetworkEnvironment* env;
 		Net::TCP::TcpAcceptor m_connectionAcceptor;
 	};
+
+	template<typename RouterT>
+	inline void HttpServer<RouterT>::run()
+	{
+		while (true)
+		{
+			auto connResult = m_connectionAcceptor.accept();
+
+			if (!connResult)
+			{
+				if (m_connectionAcceptor.isInterrupted())
+					break;
+
+				continue;
+			}
+
+			HttpSession httpSession{ std::move(connResult.value()),m_router };
+
+
+			auto action = [httpSession = std::move(httpSession)] mutable {
+				httpSession.serve();
+			};
+
+			m_threadPool.dispatch(std::move(action));
+		}
+	}
 }
+
